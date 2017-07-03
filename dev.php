3fe56bd1b9f6e9e93570dade59077ccf7528d586
NamNV609 <namnv609@gmail.com>
2017-07-03 16:31:02 +0700

    Init project

---
+++ dev.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use NNV\RestCountries;
+
+$rc = new RestCountries();
+
+\Psy\Shell::debug(get_defined_vars());
